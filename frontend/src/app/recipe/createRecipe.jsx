b81a322ee05143b24378bdc6b2fe88251449d2ba
Temi Dosu <tdosu@smu.edu>
2021-11-24 21:39:41 +0100

    Add site menu to recipe creator page and style a bit

--- frontend/src/app/recipe/createRecipe.jsx
+++ frontend/src/app/recipe/createRecipe.jsx
@@ -6,112 +6,114 @@ import { Recipe } from '../../models/Recipe';
 
 export class CreateRecipe extends React.Component {
 
-    state = {
-            id: 0,
-            recipeName: '',
-            imageURL: '',
-            rating: '',
-            instructions: '',
-            ingredients: [],
-            description: '',
-            showRecipe: false
-    };
+	state = {
+		id: 0,
+		recipeName: '',
+		imageURL: '',
+		rating: '',
+		instructions: '',
+		ingredients: [],
+		description: '',
+		showRecipe: false
+	};
 
-    //how do you get the creator/user?
-    onCreate() {
-        this.props.onRecipeAdded(this.state); /* Add to this user's 'saved'/'created' recipes */
+	//how do you get the creator/user?
+	onCreate() {
+		this.props.onRecipeAdded(this.state); /* Add to this user's 'saved'/'created' recipes */
 
-        this.setState({
-            id: 0,
-            recipeName: '',
-            imageURL: '',
-            rating: '',
-            instructions: '',
-            ingredients: [],
-            description: '',
-            showRecipe: false
-        });
-    }
+		this.setState({
+			id: 0,
+			recipeName: '',
+			imageURL: '',
+			rating: '',
+			instructions: '',
+			ingredients: [],
+			description: '',
+			showRecipe: false
+		});
+	}
 
-    removeIngredient(ing) {
-        this.setState({ showFlavor: false }); 
-        this.setState({ingredients: this.state.ingredients.filter(function(ingredient) { 
-            return ingredient !== ing 
-        })});
-    }
+	removeIngredient(ing) {
+		this.setState({ showFlavor: false });
+		this.setState({
+			ingredients: this.state.ingredients.filter(function (ingredient) {
+				return ingredient !== ing
+			})
+		});
+	}
 
-    handleCheck(ingredient) {
-        return this.state.ingredients.some(ing => ing === ingredient);
-    }
+	handleCheck(ingredient) {
+		return this.state.ingredients.some(ing => ing === ingredient);
+	}
 
-    addIngredient(ingredient) {
+	addIngredient(ingredient) {
 
-        this.setState({ showFlavor: true }); 
-        
-        if (!this.handleCheck(ingredient)) {
-        this.state.ingredients.push(ingredient);
-        }
-        else {
-            this.removeIngredient(ingredient);
-        }
-    }
-    
-    Data = ['Mustard', 'Ketchup', 'Relish', 'Milk', 'Eggs', 'Chocolate', 'Sugar'];
+		this.setState({ showFlavor: true });
 
-    render() {
-        return <>
-            <form className = "card">
-                <div className = "card-header rounded bg-danger text-light p-3 font-weight-bold h4 mt-0">Create your own recipe!</div>
-                <div className = "card-body m-2">
-                    <div className="row">
-                        <div className="col-6 ms-1 mb-3">
-                            <label htmlFor="recipeName">Recipe Name</label>
-                            <input type="text" required name="recipeName" id="recipeName" className = "form-control" value={this.state.recipeName}
-                                onChange={event => this.setState({ recipeName: event.target.value })} />
-                        </div>
+		if (!this.handleCheck(ingredient)) {
+			this.state.ingredients.push(ingredient);
+		}
+		else {
+			this.removeIngredient(ingredient);
+		}
+	}
 
-                        <div className="col-6 ms-1 mb-3">
-                            <label htmlFor="imageURL">Photo Link</label>
-                            <input type="text" required name="imageURL" id="imageURL" className = "form-control" value={this.state.imageURL}
-                                onChange={event => this.setState({ imageURL: event.target.value })} />
-                        </div>
+	Data = ['Mustard', 'Ketchup', 'Relish', 'Milk', 'Eggs', 'Chocolate', 'Sugar'];
 
-                        <div className="col-6 ms-1 mb-3">
-                        <label htmlFor="ingredients">Ingredients</label>
-                        <select name = "ingredients" className = "selectpicker form-control" multiple data-selected-text-format="count > 3" title = "Select Ingredients" onChange={event => this.addIngredient(event.target.value)}>
-                         {this.Data.map((data, i) =>
-                          <option key={i} value={data}>{data}</option>
-                                 )};
-                                </select>Ingredients selected: 
-                                {this.state.ingredients.map((ing, i) =>
-                                <span key = {i}> {ing}, </span>)}
-                        </div>
+	render() {
+		return <>
+			<form className="card container-fluid">
+				<div className="card-header rounded bg-danger text-light p-3 font-weight-bold h4 mt-0">Create your own recipe!</div>
+				<div className="card-body m-2">
+					<div className="row">
+						<div className="col-6 ms-1 mb-3">
+							<label htmlFor="recipeName">Recipe Name</label>
+							<input type="text" required name="recipeName" id="recipeName" className="form-control" value={this.state.recipeName}
+								onChange={event => this.setState({ recipeName: event.target.value })} />
+						</div>
 
-                    </div>
-                    <div className="row">
-                        <div className="col-12 ms-1 mb-3">
-                            <label htmlFor="instructions">Instructions</label>
-                            <textarea name="instructions" id="instructions" className = "form-control" value={this.state.instructions}
-                                onChange={event => this.setState({ instructions: event.target.value })}>
-                            </textarea>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12 ms-1 mb-3">
-                            <label htmlFor="description">Description</label>
-                            <textarea name="description" id="description" className = "form-control" value={this.state.description}
-                                onChange={event => this.setState({ description: event.target.value })}>
-                            </textarea>
-                        </div>
-                    </div>
-                    <div className="row mt-4 mb-4">
-                        <div className="col-12">
-                            <button onClick={() => this.onCreate()} type="button" className = "btn-lg btn-secondary bg-primary">Create</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </>;
-    }
+						<div className="col-6 ms-1 mb-3">
+							<label htmlFor="imageURL">Photo Link</label>
+							<input type="text" required name="imageURL" id="imageURL" className="form-control" value={this.state.imageURL}
+								onChange={event => this.setState({ imageURL: event.target.value })} />
+						</div>
+
+						<div className="col-6 ms-1 mb-3">
+							<label htmlFor="ingredients">Ingredients</label>
+							<select name="ingredients" className="selectpicker form-control" multiple data-selected-text-format="count > 3" title="Select Ingredients" onChange={event => this.addIngredient(event.target.value)}>
+								{this.Data.map((data, i) =>
+									<option key={i} value={data}>{data}</option>
+								)};
+							</select>Ingredients selected:
+							{this.state.ingredients.map((ing, i) =>
+								<span key={i}> {ing}, </span>)}
+						</div>
+
+					</div>
+					<div className="row">
+						<div className="col-12 ms-1 mb-3">
+							<label htmlFor="instructions">Instructions</label>
+							<textarea name="instructions" id="instructions" className="form-control" value={this.state.instructions}
+								onChange={event => this.setState({ instructions: event.target.value })}>
+							</textarea>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-12 ms-1 mb-3">
+							<label htmlFor="description">Description</label>
+							<textarea name="description" id="description" className="form-control" value={this.state.description}
+								onChange={event => this.setState({ description: event.target.value })}>
+							</textarea>
+						</div>
+					</div>
+					<div className="row mt-4 mb-4">
+						<div className="col-12">
+							<button onClick={() => this.onCreate()} type="button" className="btn-lg btn-secondary bg-primary">Create</button>
+						</div>
+					</div>
+				</div>
+			</form>
+		</>;
+	}
 
 }
