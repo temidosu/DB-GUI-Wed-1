dec171799f5c773aa1186e7808998450fdb6358b
unknown <ijoseph@smu.edu>
2021-11-24 13:11:46 -0600

    Axios added

--- frontend/src/app/Home/Home.jsx
+++ frontend/src/app/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import './Homestyle.css'
+
 //import { HomeRepository } from '../Api/homeRepository';
 
 
@@ -14,67 +15,41 @@ export class Home extends React.Component {
 
     render () {
       return <>
-         <nav>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="/createRecipe">Recipe Creator <i class="icon-chevron-down"></i></a>   
+                    <ul>
+                        <li><a href="/createRecipe">Examples</a></li>
+                        <li><a href="#">Yours</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Anouncement Board <i class="icon-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Updates</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">My Profile <i class="icon-chevron-down"></i> </a>
+                    <ul>
+                        <li><a href="#">Recipes</a></li>
+                        <li><a href="#">Account Settings</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Contact</a></li>
 
-<ul>
-   
+            </ul>
+        </nav>
 
-    <li><a href="#">Home</a></li>
-
-    <li><a href="/createRecipe">Recipe Creator <i class="icon-chevron-down"></i></a>
-      
-
-        <ul>
-
-            <li><a href="/createRecipe">Examples</a></li>
-
-            <li><a href="#">Yours</a></li>
-
-        </ul>
-
-    </li>
-
-    <li><a href="#">Anouncement Board <i class="icon-chevron-down"></i></a>
-       
-        <ul>
-
-            <li><a href="#">Updates</a></li>
-
-
-        </ul>
-
-    </li>
-
-    <li><a href="#">My Profile <i class="icon-chevron-down"></i> </a>
-        <ul>
-
-         <li><a href="#">Recipes</a></li>
-         <li><a href="#">Account Settings</a></li>
-
-
-        </ul>
-    </li>
-
-
-    <li><a href="#">Contact</a></li>
-
-</ul>
-
-</nav>
-
-
-<div>
-<div class="searchBox">
-
-<input class="searchInput"type="text" name="" placeholder="Search"/>
-<button class="searchButton" href="#">
-    <i class="material-icons">
-        search
-    </i>
-</button>
-</div>
-
-</div>
+        <div>
+            <div class="searchBox">
+                <input class="searchInput"type="text" name="" placeholder="Search"/>
+                <button class="searchButton" href="#">
+                    <i class="material-icons">
+                        search
+                    </i>
+                </button>
+            </div>
+        </div>
     </>;
   }
 
