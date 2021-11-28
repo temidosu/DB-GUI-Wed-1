abb90eadbf3a564d2e80f3bdd37de3d6fd9e13cd
Temi Dosu <tdosu@smu.edu>
2021-11-27 21:44:39 -0600

    Extract navbar into seperate component

--- frontend/src/app/Home/Home.jsx
+++ frontend/src/app/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import './Homestyle.css'
-import { CompactRecipeCard  } from '../common/CompactRecipeCard';
+import { CompactRecipeCard } from '../common/CompactRecipeCard';
 import { recipes } from '../../DummyData/recipeData';
 import { Search } from '../Search/Search';
 
@@ -11,51 +11,27 @@ import { Search } from '../Search/Search';
 
 export class Home extends React.Component {
 
-    //homeRepo = new HomeRepository();
+	//homeRepo = new HomeRepository();
 
-    state = {
-     
-    };
+	state = {
 
-    render () {
-      return <>
-       {sessionStorage.getItem("isAuthenticated") !== "true" &&
-          (<Navigate to="/login"/>)}
-        <nav>
-            <ul>
-                <li><a href="/createRecipe">Recipe Creator <i class="icon-chevron-down"></i></a>   
-                    <ul>
-                        <li><a href="/createRecipe">Examples</a></li>
-                        <li><a href="#">Yours</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Anouncement Board <i class="icon-chevron-down"></i></a>
-                    <ul>
-                        <li><a href="#">Updates</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">My Profile <i class="icon-chevron-down"></i> </a>
-                    <ul>
-                        <li><a href="#">Recipes</a></li>
-                        <li><a href="#">Account Settings</a></li>
-                    </ul>
-                </li>
-                <li><a href="/recipeSchedule">Recipe Scheduler</a></li>
-                <li><a href="#">Contact</a></li>
+	};
+
+	render() {
+		return <>
+			{sessionStorage.getItem("isAuthenticated") !== "true" &&
+				(<Navigate to="/login" />)}
 
 
-            </ul>
-        </nav>
+			<div>
+				<Search />
+			</div>
 
-        <div>
-            <Search/>
-        </div>
-        
 
-    </>;
-  }
+		</>;
+	}
 
-    
+
 }
 
 export default Home;
--- frontend/src/app/Routing.jsx
+++ frontend/src/app/Routing.jsx
@@ -4,19 +4,23 @@ import { Register } from './Accounts/Register';
 import { LoginForm } from './Accounts/LoginForm';
 import { Home } from './Home/Home';
 import { ProfilePage } from './profile/ProfilePage';
+import { Navbar } from './common/NavBar';
 //import { RecipeSchedule } from './Calendar/RecipeSchedule'
 
 
 export default function Routing({ appProps }) {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Home/>} />
-          <Route path="/login" exact element={<LoginForm/>} />
-          <Route path="/register" exact element={<Register/>} />
-          <Route path="/createRecipe" exact element={<ProfilePage/>} />
-          
-        </Routes>
-      </Router>
-    );
-  }
+	return (
+		<Router>
+
+			<Navbar></Navbar>
+
+			<Routes>
+				<Route path="/" exact element={<Home />} />
+				<Route path="/login" exact element={<LoginForm />} />
+				<Route path="/register" exact element={<Register />} />
+				<Route path="/createRecipe" exact element={<ProfilePage />} />
+
+			</Routes>
+		</Router>
+	);
+}
