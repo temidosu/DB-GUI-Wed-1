dc00f4d3bc0e49d574698f9cee35b639f708cc99
Temi Dosu <tdosu@smu.edu>
2021-11-30 16:23:23 -0600

    Hard code db values

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,16 @@ const mysql = require('mysql');
 
 // mysql connection
 var pool = mysql.createPool({
-	host: process.env.MYSQL_CLOUD_HOST,
-	user: process.env.MYSQL_CLOUD_USER,
-	password: process.env.MYSQL_CLOUD_PASS,
-	port: process.env.MYSQL_PORT,
-	database: process.env.MYSQL_DB
+	// host: process.env.MYSQL_CLOUD_HOST,
+	// user: process.env.MYSQL_CLOUD_USER,
+	// password: process.env.MYSQL_CLOUD_PASS,
+	// port: process.env.MYSQL_PORT,
+	// database: process.env.MYSQL_DB
+	host: 'dbdb.coz7ukfx1bzs.us-east-2.rds.amazonaws.com',
+	user: 'admin',
+	password: 'SSXSTAqZKwh9pApYhNMh',
+	port: 3306,
+	database: 'r2rdb'
 });
 
 // var pool = mysql.createPool({
