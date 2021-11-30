067514d5da0b66ec84f8915219d76e025cedda0d
Temi Dosu <tdosu@smu.edu>
2021-11-30 15:08:46 -0600

    Switch db.js to use AWS

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,20 @@
 const mysql = require('mysql');
 
 // mysql connection
-/*var pool = mysql.createPool({
-  host: process.env.MYSQL_CLOUD_HOST,
-  user: process.env.MYSQL_CLOUD_USER,
-  password: process.env.MYSQL_CLOUD_PASS,
-  port: process.env.MYSQL_PORT,
-  database: process.env.MYSQL_DB
-});*/
-
-
 var pool = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'warwick12',
-	port: 3306,
-	database: 'r2rdb'
+	host: process.env.MYSQL_CLOUD_HOST,
+	user: process.env.MYSQL_CLOUD_USER,
+	password: process.env.MYSQL_CLOUD_PASS,
+	port: process.env.MYSQL_PORT,
+	database: process.env.MYSQL_DB
 });
+
+// var pool = mysql.createPool({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'warwick12',
+// 	port: 3306,
+// 	database: 'r2rdb'
+// });
 
 module.exports = pool;
