97c3ac1159e9101f50a575cf2deb6b6bada7819c
Temi Dosu <tdosu@smu.edu>
2021-12-01 00:15:26 -0600

    Switch hostname

--- frontend/src/app/api/repositoryConfig.js
+++ frontend/src/app/api/repositoryConfig.js
@@ -1,4 +1,4 @@
-// export const hostname = "http://ec2-3-16-11-167.us-east-2.compute.amazonaws.com:5000";
-export const hostname = "http://localhost:5000";
+export const hostname = "http://ec2-3-16-11-167.us-east-2.compute.amazonaws.com:5000";
+// export const hostname = "http://localhost:5000";
 
 export default hostname;
