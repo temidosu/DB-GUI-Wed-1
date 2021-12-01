f032b495e7760ed78f8d911d4002f4e9bde9496f
Temi Dosu <tdosu@smu.edu>
2021-12-01 13:21:25 -0600

    Whitespace and formatting

--- frontend/src/app/Search/Search.jsx
+++ frontend/src/app/Search/Search.jsx
@@ -67,9 +67,9 @@ export class Search extends React.Component {
 
 	testLoop = () => {
 		let words = ''
-		for (let i = 0; i < this.state.data.length; i++){
-			words+= (this.state.data[i].ingredientList + ", ");
-			
+		for (let i = 0; i < this.state.data.length; i++) {
+			words += (this.state.data[i].ingredientList + ", ");
+
 		}
 		let wordArr = words.split(',');
 		console.log(wordArr)
@@ -105,32 +105,32 @@ export class Search extends React.Component {
 		}
 	}
 
-	
+
 
 	handleCheck(ingredient) {
-        return this.state.ingredientsIncluded.some(ing => ing === ingredient);
-    }
+		return this.state.ingredientsIncluded.some(ing => ing === ingredient);
+	}
 
 	removeIngredient(ing) {
-        this.setState({
-            ingredientsIncluded: this.state.ingredientsIncluded.filter(function(ingredient) { 
-            	return ingredient !== ing 
-        })
-    });
-    }
+		this.setState({
+			ingredientsIncluded: this.state.ingredientsIncluded.filter(function (ingredient) {
+				return ingredient !== ing
+			})
+		});
+	}
 
 	addIngredient(ingredient) {
 
-        
-        if (!this.handleCheck(ingredient)) {
-        	let newIng = this.state.ingredientsIncluded
+
+		if (!this.handleCheck(ingredient)) {
+			let newIng = this.state.ingredientsIncluded
 			newIng.push(ingredient);
-			this.setState({ingredientsIncluded: newIng})
-		
-        }
-        else {
-            this.removeIngredient(ingredient);
-        }
+			this.setState({ ingredientsIncluded: newIng })
+
+		}
+		else {
+			this.removeIngredient(ingredient);
+		}
 
 		/*let ingredientDataTemp = this.state.data.filter(function(item) {
 			for (var key in this.state.ingredientsIncluded) {
@@ -142,74 +142,74 @@ export class Search extends React.Component {
 		  });*/
 
 		//console.log(ingredientDataTemp)
-		
 
 
 
-    }
+
+	}
 
 	render() {
 
 		return <>
 			{sessionStorage.getItem("isAuthenticated") !== "true" &&
-      (<Navigate to="/login"/>)}
-      <div className="card">
-      <div className="search input-group container m-5">
-        <div className="searchInputs form-outline input-group">
-          <input
-		    className="form-control-lg"
-			style={{width: '400px'}}
-            type="text"
-            placeholder={this.state.placeholder}
-            value={this.state.wordEntered}
-            onChange={(e)=>this.handleFilter(e)}
-          />
-          <div className="searchIcon btn-primary float btn-lg btn fas fa-search">
-            {this.state.filteredData.length === 0 ? (
-              <SearchIcon />
-            ) : (
-              <CloseIcon id="clearBtn" onClick={(e)=>this.clearInput(e)} />
-            )}
-          </div>
-		  <div className ="mt-2 ms-2 me-4">
-			Ingredients to filter by: 
-				{this.state.ingredientsIncluded.map((ing, i) =>
-					<span key = {i}> {ing}, </span>)}
+				(<Navigate to="/login" />)}
+			<div className="card">
+				<div className="search input-group container m-5">
+					<div className="searchInputs form-outline input-group">
+						<input
+							className="form-control-lg"
+							style={{ width: '400px' }}
+							type="text"
+							placeholder={this.state.placeholder}
+							value={this.state.wordEntered}
+							onChange={(e) => this.handleFilter(e)}
+						/>
+						<div className="searchIcon btn-primary float btn-lg btn fas fa-search">
+							{this.state.filteredData.length === 0 ? (
+								<SearchIcon />
+							) : (
+								<CloseIcon id="clearBtn" onClick={(e) => this.clearInput(e)} />
+							)}
+						</div>
+						<div className="mt-2 ms-2 me-4">
+							Ingredients to filter by:
+							{this.state.ingredientsIncluded.map((ing, i) =>
+								<span key={i}> {ing}, </span>)}
+						</div>
+					</div>
+
+					<div className="mt-2 ms-0 me-4">
+
+						<label htmlFor="ingredients">Ingredients:</label>
+						<select name="ingredients" required="true" className="selectpicker form-control" multiple data-selected-text-format="count > 3" title="Select Ingredients" onChange={event => this.addIngredient(event.target.value)}>
+							{this.state.ingredientList.map((data, i) =>
+								<option key={i} value={data}>{data}</option>
+							)};
+
+						</select>
+					</div>
+
+
+
+					<label for="membership" className="mt-4 me-2"><span>Search by:</span></label>
+					<div>
+						<select className="form-select form-control mt-3" name="membership" id="membership" onChange={(e) => this.changeFilter(e)}>
+							<option
+								value="recipeFilter"
+							>Recipe</option>
+							<option
+								value="authorFilter"
+							>Author</option>
+						</select>
+
+					</div>
 				</div>
-        </div>
+				<hr />
+				<div>
 
-		<div className ="mt-2 ms-0 me-4">
-	
-			<label htmlFor="ingredients">Ingredients:</label>
-			<select name = "ingredients" required="true" className = "selectpicker form-control" multiple data-selected-text-format="count > 3" title = "Select Ingredients" onChange={event => this.addIngredient(event.target.value)}>
-				{this.state.ingredientList.map((data, i) =>
-					<option key={i} value={data}>{data}</option>
-				)};
-
-			</select>
+					{this.filterRender()}
+				</div>
 			</div>
-
-		
-        
-          <label for="membership" className="mt-4 me-2"><span>Search by:</span></label>
-		  <div>
-            <select className="form-select form-control mt-3" name="membership" id="membership" onChange={(e)=>this.changeFilter(e)}>
-              <option 
-                value="recipeFilter" 
-                >Recipe</option>
-              <option 
-                value="authorFilter"
-                >Author</option>
-            </select>
-			
-        </div>
-		</div>
-		<hr />
-		<div>
-			
-        {this.filterRender()}
-		</div>
-      </div>
 
 		</>
 	}
@@ -217,24 +217,24 @@ export class Search extends React.Component {
 		let data = await this.accountRepository.getAllRecipes()
 		this.setState({ data })
 
-	
-		let ingredientArray = []
-		for (let i = 0; i < this.state.data.length; i++){
+
+		let ingredientArray = [];
+		for (let i = 0; i < this.state.data.length; i++) {
 			let words = this.state.data[i].ingredientList;
 			let wordArr = words.split(',');
-			for (let j =0; j< wordArr.length; j++){
+			for (let j = 0; j < wordArr.length; j++) {
 				let wordPush = wordArr[j]
 				ingredientArray.push(wordPush.toLowerCase())
 			}
-			
+
 		}
 		ingredientArray.sort()
 		let uniqingredientArray = [...new Set(ingredientArray)];
-		this.setState({ingredientList: uniqingredientArray})
+		this.setState({ ingredientList: uniqingredientArray })
 
 
-	
-		
+
+
 	}
 }
 
--- frontend/src/app/common/CompactRecipeCard.jsx
+++ frontend/src/app/common/CompactRecipeCard.jsx
@@ -1,85 +1,83 @@
 import React from "react";
-import {Link, NavLink} from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 
 
 
 export class CompactRecipeCard extends React.Component {
-    
-setRecipeID(e){
-    sessionStorage.setItem("recipeID", this.props.card.recipeID);
-    console.log(sessionStorage.getItem("recipeID"))
-}
 
-/*conditionEdit(){
-    if(this.props.card.userID === sessionStorage.getItem("userId")){
-        console.log("HEYYYY")
-        console.log(this.props.card.userID, sessionStorage.getItem("userId"))
-        return <>
-            <button type="button" className = "float-end mt-1 mb-2 btn btn-sm btn-info" style= {{width: '80px'}}>
-								<NavLink to ={ '/editRecipe'} style = {{textDecoration: 'none', color: 'black'}}>Edit</NavLink></button>
-        </>
-    }
-}*/
+	setRecipeID(e) {
+		sessionStorage.setItem("recipeID", this.props.card.recipeID);
+		console.log(sessionStorage.getItem("recipeID"))
+	}
 
-
-    render() {
-        return <>
+	/*conditionEdit(){
+		if(this.props.card.userID === sessionStorage.getItem("userId")){
+			console.log("HEYYYY")
+			console.log(this.props.card.userID, sessionStorage.getItem("userId"))
+			return <>
+				<button type="button" className = "float-end mt-1 mb-2 btn btn-sm btn-info" style= {{width: '80px'}}>
+									<NavLink to ={ '/editRecipe'} style = {{textDecoration: 'none', color: 'black'}}>Edit</NavLink></button>
+			</>
+		}
+	}*/
 
 
-    <div id="CompactRecipeCard">
-        <li className="list-group mb-0 p-0"></li>
-        {
-            //  !props.createdRecipes.length && <li className= "list-group-item">No recipe created yet!</li>
-        }
-        {
-            //works but this is incomplete - only show 'edit' button if the user logged in created the recipe
-            <li className="card rounded container mb-4 list-group p-0 border border-danger">
-                
-                <div>
-				<h2 className="h2 card-title card-header bg-danger text-light" style={{background: 'red'}}>
-                            <a href="/viewRecipe" style = {{textDecoration: 'none', color: 'white'}} onClick={e=> this.setRecipeID(e)}>{this.props.card.recipeName}</a>
-                            <button type="button" className = "float-end mt-1 mb-2 btn btn-sm btn-info" style= {{width: '80px'}}>
-								<NavLink to ={ '/editRecipe'} style = {{textDecoration: 'none', color: 'black'}}>Edit</NavLink></button>
-                        </h2>
+	render() {
+		return <>
+			<div id="CompactRecipeCard">
+				<li className="list-group mb-0 p-0"></li>
+				{
+					//  !props.createdRecipes.length && <li className= "list-group-item">No recipe created yet!</li>
+				}
+				{
+					//works but this is incomplete - only show 'edit' button if the user logged in created the recipe
+					<li className="card rounded container mb-4 list-group p-0 border border-danger">
 
-						<h5 className="h5 card-header">
-                           Rating placeholder
-                        </h5>
-				
-                    {/* [1, 2, 3, 4, 5].map(x => (<i key={x} className={(x > this.props.card.rating ? 'empty-star' : 'full-star')}></i>)) */}
-                </div>
-				<img className = "card-img-top" style={{height: '300px', width: '100%'}} src = {this.props.card.hyperlink} alt= "Recipe image"></img>
-			
-                <div className="card-body p-0">
-                    <div className="row mt-1 mb-1 ms-1">
-					
-						<div className = "h4 mt-1">
-						
-						<h4>Instructions:</h4>
-						<div className="row mt-1 mb-1 ms-1">
-                        <div className="card-text text-muted h6">{this.props.card.recipeIndc}</div>
-                        </div>
+						<div>
+							<h2 className="h2 card-title card-header bg-danger text-light" style={{ background: 'red' }}>
+								<a href="/viewRecipe" style={{ textDecoration: 'none', color: 'white' }} onClick={e => this.setRecipeID(e)}>{this.props.card.recipeName}</a>
+								<button type="button" className="float-end mt-1 mb-2 btn btn-sm btn-info" style={{ width: '80px' }}>
+									<NavLink to={'/editRecipe'} style={{ textDecoration: 'none', color: 'black' }}>Edit</NavLink></button>
+							</h2>
 
-						<h4>Description:</h4>
-                    <div className="row mt-1 mb-1 ms-1">
-                        <div className="text-muted card-text h6">"{this.props.card.recipeDesc}"</div>
-                    </div>
+							<h5 className="h5 card-header">
+								Rating placeholder
+							</h5>
 
+							{/* [1, 2, 3, 4, 5].map(x => (<i key={x} className={(x > this.props.card.rating ? 'empty-star' : 'full-star')}></i>)) */}
 						</div>
-			
-					</div>
-				
-                    <div>
+						<img className="card-img-top" style={{ height: '300px', width: '100%' }} src={this.props.card.hyperlink} alt="Recipe image"></img>
 
-                    </div>
-                </div>
-				<div className = "card-footer mb-0">
-                        <h5>Recipe Creator: {this.props.card.recipeCreator}</h5>
-                        </div>
-            </li>
-        }</div>
-</>
+						<div className="card-body p-0">
+							<div className="row mt-1 mb-1 ms-1">
 
-    }
+								<div className="h4 mt-1">
+
+									<h4>Instructions:</h4>
+									<div className="row mt-1 mb-1 ms-1">
+										<div className="card-text text-muted h6">{this.props.card.recipeIndc}</div>
+									</div>
+
+									<h4>Description:</h4>
+									<div className="row mt-1 mb-1 ms-1">
+										<div className="text-muted card-text h6">"{this.props.card.recipeDesc}"</div>
+									</div>
+
+								</div>
+
+							</div>
+
+							<div>
+
+							</div>
+						</div>
+						<div className="card-footer mb-0">
+							<h5>Recipe Creator: {this.props.card.recipeCreator}</h5>
+						</div>
+					</li>
+				}</div>
+		</>
+
+	}
 }
